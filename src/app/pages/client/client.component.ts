50bb9925a971209a553ce92250a324f74b7d2594
Dávisson Falcão <davissonbeat@hotmail.com>
2025-05-12 16:21:56 -0300

    feat: adicionar funcionalidadse na client component

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from '../../shared/services/client.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import Papa from 'papaparse';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,8 @@ export class ClientComponent implements OnInit {
   pageSize: number = 6;
   totalPages: number = 0;
   allClientes: any[] = [];
+  tabelaVisivel: boolean = true;
+  loading: boolean = false;
 
   constructor(private clientService: ClientService) {}
 
@@ -27,15 +30,18 @@ export class ClientComponent implements OnInit {
   }
 
   loadClientes(): void {
+    this.loading = true;
     this.clientService.getClientes().subscribe(
       (response) => {
         this.allClientes = response; // Armazenar todos os clientes recebidos
         this.totalClientes = this.allClientes.length;
         this.totalPages = Math.ceil(this.totalClientes / this.pageSize); // Calcular o total de páginas
-        this.updatePageData();
+        this.updatePageData(); // Atualiza os dados da página
+        this.loading = false;
       },
       (error) => {
         console.error('Erro ao carregar clientes', error);
+        this.loading = false;
       }
     );
   }
@@ -56,5 +62,27 @@ export class ClientComponent implements OnInit {
       this.currentPage = page;
       this.updatePageData();
     }
+  }
+
+  // Alternar status do cliente
+  toggleStatus(cliente: any): void {
+    cliente.status = cliente.status === 'ativo' ? 'inativo' : 'ativo';
+    this.updatePageData(); // Atualiza os dados da página após a mudança de status
+  }
+
+  // Exportar lista de clientes para CSV
+  exportarCSV(): void {
+    const csvData = Papa.unparse(this.allClientes); // Converte os dados em CSV usando a biblioteca PapaParse
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'clientes.csv');
+    link.click(); // Dispara o download do CSV
+  }
+
+  // Alternar visibilidade da tabela de clientes
+  toggleTabela(): void {
+    this.tabelaVisivel = !this.tabelaVisivel;
   }
 }
